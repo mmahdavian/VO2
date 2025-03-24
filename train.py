450e7b2054c66a3d56a6962ee1ab4f5a7a2cf4ab
mmahdavian <mmahdavi@sfu.ca>
2025-03-23 18:06:43 -0700

    update

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ def get_parser():
     parser.add_argument('--future_data', default=32, type=int)
     parser.add_argument('--interval', default=1, type=int)
     parser.add_argument('--time_interval', default=1, type=int)
-    parser.add_argument('--model_name', default='NN_mixed_deep', type=str)
+    parser.add_argument('--model_name', default='NN_mixed_shallow', type=str)
     parser.add_argument('--wandb', default=False, type=bool)
     parser.add_argument('--wandb_name', default='Zepp', type=str)
     parser.add_argument('--kernel_len', default=3, type=int)
